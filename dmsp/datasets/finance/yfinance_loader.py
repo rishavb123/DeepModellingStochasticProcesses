be2f897c53bd80b069d76b758d8932a5b1d842ba
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-02 16:57:28 -0400

    added yfinance loader

---
+++ dmsp/datasets/finance/yfinance_loader.py
@@ -0,0 +1,45 @@
+"""Data loader for the yfinance module"""
+
+from typing import List, Tuple
+
+import numpy as np
+import yfinance as yf
+
+from dmsp.datasets.base_loader import BaseLoader
+
+
+class YFinanceLoader(BaseLoader):
+    """Yahoo Finance data loader class."""
+
+    def __init__(self, symbols: List[str], intervals: List[Tuple[str, str]]) -> None:
+        super().__init__(
+            f"./data/finance/{'_'.join(symbols)}_{'_'.join([f'{start}to{end}' for (start, end) in intervals])}/"
+        )
+        self.symbols = symbols
+        self.intervals = intervals
+
+    def _download_data(self) -> List[np.ndarray]:
+        data = []
+        for start, end in self.intervals:  # Loop through each interval
+            df = yf.download(
+                self.symbols, start=start, end=end
+            )  # Download data from yfinance
+            df = df[["Adj Close", "Volume"]]  # Extract price and volume information
+            data.append(df.to_numpy())
+
+        return data
+
+
+if __name__ == "__main__":
+    loader = YFinanceLoader(
+        symbols=["SPY", "AAPL"],
+        intervals=[
+            ("2022-01-01", "2022-12-31"),
+            ("2019-01-01", "2021-12-31"),
+            ("2023-01-01", "2023-12-31"),
+        ],
+    )
+    loader.load()
+    print(loader.data)
+    for d in loader.data:
+        print(d.shape)
