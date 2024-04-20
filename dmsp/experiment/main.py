0c76f9bf9070ab3062288c9ef8730be592f19518
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-20 01:25:48 -0400

    added eval resolver

--- dmsp/experiment/main.py
+++ dmsp/experiment/main.py
@@ -1,15 +1,25 @@
 """The main experiment entry point."""
 
+from omegaconf import OmegaConf
+
 from experiment_lab.core import run_experiment
+from experiment_lab.common.resolvers import register_resolvers
 
 from dmsp.experiment.config import DMSPConfig, register_configs
 from dmsp.experiment.experiment import DMSPExperiment
+
+
+def register_dmsp_resolvers():
+    register_resolvers()
+    OmegaConf.register_new_resolver("eval", eval)
+
 
 if __name__ == "__main__":
     run_experiment(
         experiment_cls=DMSPExperiment,
         config_cls=DMSPConfig,
         register_configs=register_configs,
+        register_resolvers=register_dmsp_resolvers,
         config_path=f"./configs",
         config_name="dmsp",
     )
