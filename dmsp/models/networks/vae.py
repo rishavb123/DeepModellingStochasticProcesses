b9a2d0c9838fd5e215dd5b1f557c74cbd30e6db6
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-23 23:33:17 -0400

    working on vaes

---
+++ dmsp/models/networks/vae.py
@@ -0,0 +1,104 @@
+from typing import Any, Tuple
+
+import torch
+import torch.nn as nn
+
+
+class VAE(nn.Module):
+
+    def __init__(
+        self,
+        encoder: nn.Module,
+        decoder: nn.Module,
+        latent_dim: int,
+        reconstruction_loss_f: nn.Module | None = None,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.encoder = encoder
+        self.decoder = decoder
+        self.latent_dim = latent_dim
+
+        self.reconstruction_loss_f = (
+            nn.BCELoss() if reconstruction_loss_f is None else reconstruction_loss_f
+        )
+
+    def set_device(self, device: str | None):
+        self.device = None if device is None else torch.device(device=device)
+        self.encoder = self.encoder.to(self.device)
+        self.decoder = self.decoder.to(self.device)
+        self.reconstruction_loss_f = self.reconstruction_loss_f.to(self.device)
+
+    def reparameterization(self, mean: torch.Tensor, std: torch.Tensor) -> torch.Tensor:
+        epsilon = torch.randn_like(mean, device=self.device)
+        return mean + std * epsilon
+
+    def sample_noise(self, n_samples: int = 1) -> torch.Tensor:
+        return torch.randn((n_samples, self.latent_dim), device=self.device)
+
+    def sample(self, n_samples: int = 1):
+        with torch.no_grad():
+            noise = self.sample_noise(n_samples=n_samples)
+            return self.decoder(noise)
+
+    def loss(self, x: torch.Tensor) -> torch.Tensor:
+        x_hat, mean, logvar = self.forward(x=x)
+        reconstruction_loss = self.reconstruction_loss_f(x_hat, x)
+        kl_divergence_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
+        return reconstruction_loss + kl_divergence_loss
+
+    def forward(
+        self, x: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        mean, logvar = self.encoder(x)
+        std = torch.exp(0.5 * logvar)
+        z = self.reparameterization(mean=mean, std=std)
+        x_hat = self.decoder(z)
+        return x_hat, mean, logvar
+
+
+class ConditionedVAE(VAE):
+
+    def __init__(
+        self,
+        encoder: nn.Module,
+        decoder: nn.Module,
+        latent_dim: int,
+        reconstruction_loss_f: nn.Module | None = None,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            encoder, decoder, latent_dim, reconstruction_loss_f, *args, **kwargs
+        )
+
+    def sample_noise(self, n_samples: int = 1) -> torch.Tensor:
+        return torch.randn((n_samples, self.latent_dim), device=self.device)
+
+    def sample(self, x: torch.Tensor, n_samples: int = 1):
+        batch_size, _ = x.shape
+        with torch.no_grad():
+            x = x.repeat((n_samples, 1, 1)).swapaxes(
+                0, 1
+            )  # (batch_size, n_samples, lookback * d)
+            noise = self.sample_noise(n_samples=batch_size * n_samples).reshape(
+                (batch_size, n_samples, self.latent_dim)
+            )  # (batch_size, n_samples, latent_dim)
+            return self.decoder((noise, x))  # (batch_size, n_samples, d)
+
+    def loss(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        y_hat, mean, logvar = self.forward(x=x, y=y)
+        reconstruction_loss = self.reconstruction_loss_f(y_hat, y)
+        kl_divergence_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
+        return reconstruction_loss + kl_divergence_loss
+
+    def forward(
+        self, x: torch.Tensor, y: torch.Tensor
+    ) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        mean, logvar = self.encoder((x, y))
+        std = torch.exp(0.5 * logvar)
+        z = self.reparameterization(mean=mean, std=std)
+        y_hat = self.decoder((z, x))
+        return y_hat, mean, logvar
--- dmsp/models/trainers/vae_trainer.py
+++ dmsp/models/trainers/vae_trainer.py
@@ -1,24 +1,69 @@
-from typing import Dict, List
+from typing import Any, Dict, List
 
 import numpy as np
-from torch import Tensor
-from torch.utils.data.dataset import Dataset
+import hydra
+import torch
+import torch.utils.data.dataset
 
 from dmsp.models.trainers.base_trainer import BaseTrainer
+from dmsp.models.networks.vae import ConditionedVAE
 
 
-class VAETrainer(BaseTrainer):
+class ConditionalVAETrainer(BaseTrainer):
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        lookback: int,
+        vae: ConditionedVAE,
+        optimizer_cls: str = "torch.optim.Adam",
+        optimizer_kwargs: Dict[str, Any] | None = None,
+        device: str = "cpu",
+        dtype: torch.dtype = torch.float32,
+    ) -> None:
         super().__init__()
+        self.lookback = lookback
 
-    def preprocess(self, trajectory_list: List[np.ndarray]) -> Dataset:
-        return super().preprocess(trajectory_list)
+        self.device = torch.device(device)
+        self.dtype = dtype
+
+        self.optimizer: torch.optim.Optimizer = hydra.utils.instantiate(
+            {
+                "_target_": optimizer_cls,
+                **({} if optimizer_kwargs is None else optimizer_kwargs),
+            },
+            params=self.prediction_model.parameters(),
+            _convert_="partial",
+        )
+
+        self.vae = vae.to(device=self.device)
+
+    def preprocess(
+        self, trajectory_list: List[np.ndarray]
+    ) -> torch.utils.data.dataset.Dataset:
+        X = []
+        y = []
+
+        for traj in trajectory_list:
+            for t in range(self.lookback + 1, traj.shape[0]):
+                X.append(np.diff(traj[t - self.lookback - 1 : t, :], axis=0).flatten())
+                y.append(traj[t, :] - traj[t - 1, :])
+
+        X = np.array(X)
+        y = np.array(y)
+
+        X = torch.tensor(X, device=self.device, dtype=self.dtype)
+        y = torch.tensor(y, device=self.device, dtype=self.dtype)
+
+        return torch.utils.data.TensorDataset(X, y)
 
     def validate_traj_lst(
         self, trajectory_list: List[np.ndarray], sample_from_lookback: int = 0
     ) -> List[np.ndarray]:
-        return super().validate_traj_lst(trajectory_list, sample_from_lookback)
+        return [
+            traj
+            for traj in trajectory_list
+            if traj.shape[0] > self.lookback + sample_from_lookback
+        ]
 
     def sample(
         self,
@@ -27,18 +72,72 @@ class VAETrainer(BaseTrainer):
         traj_length: int = 1,
         sample_from_lookback: int = 0,
     ) -> List[np.ndarray]:
-        return super().sample(
-            trajectory_list, n_samples, traj_length, sample_from_lookback
-        )
+        n_traj = len(trajectory_list)
+        d = trajectory_list[0].shape[1]
+
+        if sample_from_lookback == 0:
+            X = [
+                np.diff(traj[-self.lookback - 1 :, :], axis=0).flatten()
+                for traj in trajectory_list
+            ]
+        else:
+            X = [
+                np.diff(
+                    traj[
+                        -self.lookback
+                        - 1
+                        - sample_from_lookback : -sample_from_lookback,
+                        :,
+                    ],
+                    axis=0,
+                ).flatten()
+                for traj in trajectory_list
+            ]
+        X = np.array(X)
+        X = torch.tensor(X, device=self.device, dtype=self.dtype).swapaxes(
+            0, 1
+        )  # (n_traj, lookback * d)
+
+        samples = np.zeros((n_traj, n_samples, 1 + traj_length, d))
+
+        for i, traj in enumerate(trajectory_list):
+            if sample_from_lookback == 0:
+                samples[i, :, 0, :] = np.repeat(traj[-1:, :], repeats=n_samples, axis=0)
+            else:
+                samples[i, :, 0, :] = np.repeat(
+                    traj[-1 - sample_from_lookback : -sample_from_lookback, :],
+                    repeats=n_samples,
+                    axis=0,
+                )
+
+        for t in range(1, 1 + traj_length):
+            with torch.no_grad():
+                yhat: torch.Tensor = self.vae.sample(x=X)  # (n_traj, n_samples, d)
+                samples[:, :, t, :] = yhat.detach().cpu().numpy()
+                X[:, :, :-d] = X[:, :, d:]
+                X[:, :, -d:] = yhat
+
+        return list(samples.cumsum(axis=2)[:, :, 1:, :])
 
     def load_model(self, path: str) -> None:
-        return super().load_model(path)
+        self.vae.load_state_dict(torch.load(path))
 
     def save_model(self, path: str) -> None:
-        return super().save_model(path)
+        torch.save(self.vae.state_dict(), path)
 
-    def train(self, train_batch: Tensor | List[Tensor]) -> Dict[str, float]:
-        return super().train(train_batch)
+    def train(self, train_batch: torch.Tensor | List[torch.Tensor]) -> Dict[str, float]:
+        self.optimizer.zero_grad()
 
-    def eval(self, eval_batch: Tensor | List[Tensor]) -> Dict[str, float]:
-        return super().eval(eval_batch)
+        X, y = train_batch
+
+        loss = self.vae.loss(x=X, y=y)
+        loss.backward()
+        self.optimizer.step()
+
+        return {"train/loss": loss.item()}
+
+    def eval(self, eval_batch: torch.Tensor | List[torch.Tensor]) -> Dict[str, float]:
+        with torch.no_grad():
+            X, y = eval_batch
+            loss = self.vae.loss(x=X, y=y)
+        return {"eval/loss": loss.item()}
