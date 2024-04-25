a624b9ba7929879b36249e330db155d720e5657f
Erik Scarlatescu <saerik2001@gmail.com>
2024-04-25 03:12:49 -0400

    Got rid of erroneous debug statement

--- dmsp/utils/process_data.py
+++ dmsp/utils/process_data.py
@@ -15,7 +15,6 @@ def validate_traj_list(
 def preprocess(trajectory_list: List[np.ndarray], device, dtype, stream_data, lookback: int, lookforward: int = 1) -> torch.utils.data.Dataset:
     X = []
     y = []
-    import pdb; pdb.set_trace()
 
     for traj in trajectory_list:
         for t in range(lookback + 1, traj.shape[0] - lookforward):
