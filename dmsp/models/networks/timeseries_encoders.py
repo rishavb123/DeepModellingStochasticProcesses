c59a14b2e6893544026960d930f1901e5cd46c23
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-26 21:20:00 -0400

    added timeseries encoder file

--- dmsp/models/networks/timeseries_encoders.py
+++ dmsp/models/networks/timeseries_encoders.py
@@ -1,12 +1,27 @@
+from typing import Any, Callable
+
 import torch
 import torch.nn as nn
 
 
-class LSTMEncoder(nn.LSTM):
+class TimeseriesEncoder(nn.Module):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(
+        self,
+        data_dim: int,
+        encoder: nn.Module,
+        post_process_f: Callable[[Any], torch.Tensor] | None,
+        *args,
+        **kwargs,
+    ) -> None:
         super().__init__(*args, **kwargs)
+        self.data_dim = data_dim
+        self.encoder = encoder
+        self.post_process_f = (
+            (lambda x: x) if post_process_f is None else post_process_f
+        )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        output, _hidden = super().forward(x)
-        return output
+        return self.post_process_f(
+            self.encoder(x.reshape((x.shape[0], -1, self.data_dim)))
+        )
