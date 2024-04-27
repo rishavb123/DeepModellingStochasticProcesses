cc3de4688a13613fe960fe6ce7108c2f4783b7ba
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-27 19:09:35 -0400

    removed pdb

--- dmsp/utils/output_shape_calculators.py
+++ dmsp/utils/output_shape_calculators.py
@@ -1,14 +1,19 @@
 import torch
 
+
 def calc_encoder_output_size(module_lst, input_sizes, aggregator):
     print("Calling function to calculate output shape:")
     print(module_lst, input_sizes)
 
-    assert len(module_lst) == len(input_sizes), "Length of module lists and input sizes must be the same."
+    assert len(module_lst) == len(
+        input_sizes
+    ), "Length of module lists and input sizes must be the same."
 
-    import pdb; pdb.set_trace()
     inp_tensors = [torch.zeros((1, inp_size)) for inp_size in input_sizes]
-    out_tensor = aggregator([module(inp_tensor) for module, inp_tensor in zip(module_lst, inp_tensors)], dim=-1)
+    out_tensor = aggregator(
+        [module(inp_tensor) for module, inp_tensor in zip(module_lst, inp_tensors)],
+        dim=-1,
+    )
 
     out_size = out_tensor.flatten().shape[0]
 
