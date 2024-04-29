2b69affbf512f1ca20dac89a49a4d6ff1f3916a4
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2024-04-28 23:44:56 -0400

    added more stocks

--- dmsp/datasets/finance/yfinance_loader.py
+++ dmsp/datasets/finance/yfinance_loader.py
@@ -3,6 +3,7 @@
 from typing import Any, Dict, List
 
 import numpy as np
+import pandas as pd
 import yfinance as yf
 
 from dmsp.datasets.base_loader import BaseLoader
@@ -17,6 +18,7 @@ class YFinanceLoader(BaseLoader):
         download_kwargs: List[Dict[str, Any]],
         columns: List[str] | None = None,
         normalize_by_first_price: bool = False,
+        query_separately: bool = False,
     ) -> None:
         """Constructor for the yfinance loader.
 
@@ -25,6 +27,7 @@ class YFinanceLoader(BaseLoader):
             download_kwargs (List[Dict[str, Any]]): The kwargs to use for each trajectory.
             columns (List[str] | None, optional): The columns from the yfinance return to use. Defaults to None.
             normalize_by_first_price (bool, optional): Whether or not to normalize the data by the first price downloaded. Defaults to False.
+            query_separately (bool, optional): Whether or not to query each symbol separately. Defaults to false.
         """
         self.symbols = symbols
         self.columns = ["Adj Close", "Volume"] if columns is None else columns
@@ -39,6 +42,7 @@ class YFinanceLoader(BaseLoader):
         )
         self.download_kwargs = download_kwargs
         self.normalize_by_first_price = normalize_by_first_price
+        self.query_separately = query_separately
 
     def _download_data(self) -> List[np.ndarray]:
         """Downloads the dataset using the yfinance download function.
@@ -48,25 +52,49 @@ class YFinanceLoader(BaseLoader):
         """
         data = []
         for download_kwargs in self.download_kwargs:
-            df = yf.download(
-                self.symbols, **download_kwargs
-            )  # Download data from yfinance
-            df = df[self.columns]  # Extract price and volume information
-            df = df / df.iloc[0]
+            if self.query_separately:
+                df = []
+                for symbol in self.symbols:
+                    sym_df = yf.download([symbol], **download_kwargs)
+                    sym_df = sym_df[self.columns]
+                    sym_df.columns = [
+                        f"{symbol}_{col_name}" for col_name in sym_df.columns
+                    ]
+                    df.append(sym_df)
+                df = pd.concat(df, axis=1).dropna()
+            else:
+                df = yf.download(
+                    self.symbols, **download_kwargs
+                )  # Download data from yfinance
+                df = df[self.columns]  # Extract price and volume information
+            if self.normalize_by_first_price:
+                df = df / df.iloc[0]
             data.append(df.to_numpy())
         return data
 
 
 if __name__ == "__main__":
     loader = YFinanceLoader(
-        symbols=["SPY", "AMZN", "MSFT", "AAPL", "TSLA", "WM", "STEEL"],
+        symbols=[
+            "SPY",
+            "AMZN",
+            "MSFT",
+            "AAPL",
+            "TSLA",
+            "WM",
+            "STEEL",
+            "VIX",
+            "USDJPY=X",
+            "JPYUSD=X",
+        ],
         download_kwargs=[
             {"start": "2010-01-01", "end": "2023-12-31", "interval": "1d"},
         ],
         columns=["Adj Close"],
         normalize_by_first_price=True,
+        query_separately=True,
     )
-    loader.load()
+    loader.load(force_redownload=True)
     for d in loader.data:
         print(d.shape)
 
